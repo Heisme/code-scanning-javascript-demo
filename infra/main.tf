462341ea0e43a1d765e2bd32f1aba24801314953
Heisme <yeunnah@gmail.com>
2022-11-04 11:41:09 +0800

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -3,6 +3,8 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_client_config" "current" {}
+
 # Create a Resource Group if it doesn’t exist
 resource "azurerm_resource_group" "tfexample" {
   name     = "my-terraform-rg"
@@ -14,4 +16,14 @@ resource "azurerm_virtual_network" "tfexample" {
   location            = azurerm_resource_group.tfexample.location
   resource_group_name = azurerm_resource_group.tfexample.name
   address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                    = "abc"
+  location                  = azurerm_resource_group.tfexample.location
+  resource_group_name       = azurerm_resource_group.tfexample.name
+  purge_protection_enabled  = false
+  sku_name                  = "standard"
+  enabled_for_disk_encryption = false
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
 }
